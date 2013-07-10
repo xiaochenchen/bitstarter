47d48898130f609fc535f8493bd86970f5f834a4
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-07-10 13:42:20 +0000

    changed web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
