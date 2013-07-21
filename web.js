095452d41d53fc3de0489dc00b51e328450e2fca
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-07-21 04:20:13 +0000

    corrected a line referencing express based on a warning

--- web.js
+++ web.js
@@ -3,13 +3,14 @@ var express = require('express');
 var fs = require('fs');
 var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   response.send(buf.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
