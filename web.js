b5e85adc03ff74d81514ed7442edbb0716cc0355
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-04 16:56:06 -0400

    fix express in web.js

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var fs = require('fs');
 var buf = new Buffer(fs.readFileSync('index.html'), "utf-8");
 
-//var app = express.createServer(express.logger());
-var app = express();
+var app = express.createServer(express.logger());
+//var app = express();
 
 app.get('/', function(request, response) {
   response.send(buf.toString("utf-8"));
